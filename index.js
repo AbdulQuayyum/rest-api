8a1a123f15ffff09b96d286447275e028c981f94
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2022-04-24 23:46:28 +0100

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from 'body-Parser';
 
-const app = express();
+const app = express(); 
 const PORT = 5500;
 
 app.use(bodyParser.json());
 
-app.listen(PORT, () => console.log(`Server is running on port: http://locathost:${PORT} `))
+app.get('/', (req, res) => res.send('Hello from Homepage.'))
+
+app.listen(PORT, () => console.log(`Server is running on port: http://localhost:${PORT} `))
