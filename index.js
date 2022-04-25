16c44cc2e1cab888e5e0a28eee37de2dcd292adb
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2022-04-25 02:30:58 +0100

    first commit

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const PORT = 5500;
 
 app.use(bodyParser.json());
 
-app.use('/users' , usersRoutes)
-
-app.get('/', (req, res) => res.send('Hello from Homepage.'))
+app.use("/people", usersRoutes);
+app.get("/", (req, res) => res.send("Welcome to the Users API!"));
+app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
 
 app.listen(PORT, () => console.log(`Server is running on port: http://localhost:${PORT} `))
--- routes/users.js
+++ routes/users.js
@@ -1,51 +1,17 @@
-import express from "express";
-import { v4 as uuidv4 } from 'uuid';
+import express from 'express';
+
+import { getUsers, createUser, getUser, deleteUser, updateUser } from '../controllers/users.js';
 
 const router = express.Router();
 
-let users = []
+router.get('/', getUsers);
 
-// all routes in here are starting with /users
-router.get('/', (req, res) => {
-    res.send(users)
-})
+router.post('/', createUser);
 
-router.post('/', (req, res) => {
-    const user = req.body;
+router.get('/:id', getUser);
 
-    users.push({...user, id: uuidv4() })
+router.delete('/:id', deleteUser);
 
-    res.send( `User with the name ${user.firstName} was added to the database` )
-})
-
-//req.params is used to get the id
-router.get('/:id', (req, res) => {
-    const { id } = req.params
-
-    const foundUser = users.find((user) => user.id = id)
-
-    res.send(foundUser)
-})
-
-router.delete('/:id', (req, res) => {
-    const { id } = req.params 
-
-    users = users.filter((user) => user.id != id )
-
-    res.send(`User with the id ${id} was  deleted from database. `)
-})
-
-router.patch('/:id', (req, res) => {
-     const { id } = req.params;
-     const { firstName, lastName, age } = req.body
-     
-     const user = users.find((user) => user.id == id)
-
-     if(firstName) user.firstName = firstName
-     if(lastName) user.lastName = lastName
-     if(age) user.age = age
-
-     res.send(`User with the id ${id} has been updated`)
-})
+router.patch('/:id', updateUser);
 
 export default router;
