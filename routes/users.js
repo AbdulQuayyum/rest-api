7407499c7377261af341e6c0adc0ca9406846988
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2022-04-25 01:12:42 +0100

    first commit

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,9 @@
 import express from "express";
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [
-    {
-        fristName: "Quayyum",
-        lastName: "Alao",
-        age: 17
-    },
-    {
-        fristName: "John",
-        lastName: "Doe",
-        age: 25
-    }
-]
+const users = []
 
 // all routes in here are starting with /users
 router.get('/', (req, res) => {
@@ -23,9 +13,20 @@ router.get('/', (req, res) => {
 router.post('/', (req, res) => {
     const user = req.body;
 
-    users.push(user)
+    users.push({...user, id: uuidv4() })
 
     res.send( `User with the name ${user.fristName} was added to the database` )
 })
+
+//req.params is used to get the id
+router.get('/:id', (req, res) => {
+    const { id } = req.params
+
+    const foundUser = users.find((user) => user.id = id)
+
+    res.send(foundUser)
+})
+
+router.delete('/:')
 
 export default router;
